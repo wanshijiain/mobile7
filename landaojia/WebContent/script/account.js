9bca92314f479bff1f60b1692defcb8b82557e7b
Jason <lidongxuan1991@gmail.com>
2015-07-29 23:20:43 +0800

    update

--- landaojia/WebContent/script/account.js
+++ landaojia/WebContent/script/account.js
@@ -2,4 +2,10 @@ $(document).on('click', '.menu', function(){
 	if($(this).hasClass('order')){
 		window.location.href='/order/order.html';
 	}
+	if($(this).hasClass('address')){
+		currentView.router.load({
+			url:'/account/addressMgr.html'
+		})
+	}
+	app.hideToolbar('.toolbar');
 });
